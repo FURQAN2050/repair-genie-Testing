485afc2fa93383ed2e3318e9554338ef07b718d8
Arsalan <mohdarsalan318@gmail.com>
2019-10-30 23:57:51 +0500

    Barcode done

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from driverLogin import driver
 import submitWorkOrder
 from submitWorkOrder import submitWorkOrder
 
+from test_barcode import BarcodeTest
+
 import chromeWebDriver
 from chromeWebDriver import chromeWebDriver
 
@@ -17,8 +19,12 @@ if __name__ == '__main__':
     cwdObj.setchromeWebDriver()     #set chromedriver
     webdriver=cwdObj.getWebDriver() #get chrome driver so that the same instance should be send to all files
 
-    loginObj=login(webdriver,"stech","pass")
-    swo=submitWorkOrder(webdriver)
+    # loginObj=login(webdriver,"stech","pass")
+    # swo=submitWorkOrder(webdriver)
+
+    loginObj=login(webdriver,"admin","pass")
+    barcodeObj=BarcodeTest(webdriver)
+
     driver = driver(webdriver,"driver","pass")
     webdriver.quit()
 
---
+++ test_barcode.py
@@ -0,0 +1,68 @@
+import time
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
+class BarcodeTest:
+    def __init__(self,wd):
+        self.driver = wd
+        self.runTest()
+    
+    def openBarcodePage(self):
+            time.sleep(2)
+            self.driver.get("http://testing.repairgenie.net/barcode")
+            print('Admin Dashboard succesfully open')
+    
+    def enterBarcodeNumber(self):
+            time.sleep(2)
+            self.driver.find_element(By.ID, "code").click()
+            self.driver.find_element(By.ID, "code").send_keys("12345-S")
+            self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+            print('Barcode Entered Successfully')
+    
+    def fillTextBoxes(self):
+        self.fillLenovoNotes()
+        self.fillSchoolNotes
+        self.fillPrivateNotes()
+        self.fillRepairProvided()
+        self.clickStatusButton()
+    
+    def fillLenovoNotes(self):
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "lennotes").click()
+        self.driver.find_element(By.NAME, "lennotes").send_keys("Lenovo Notes")
+        print('Lenovo Notes filled Successfully')
+
+    def fillSchoolNotes(self):
+        time.sleep(1)
+        self.driver.find_element(By.ID, "notes").click()
+        self.driver.find_element(By.ID, "notes").send_keys("School Notes")
+        print('School Notes filled Successfully')
+    
+    def fillPrivateNotes(self):
+        time.sleep(1)
+        self.driver.find_element(By.ID, "privnotes").click()
+        self.driver.find_element(By.ID, "privnotes").send_keys("Private Notes")
+        print('Private Notes filled Successfully')
+    
+    def fillRepairProvided(self):
+        time.sleep(1)
+        self.driver.find_element(By.CSS_SELECTOR, ".chosen-choices").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//div/div/div/ul/li[7]").click()
+        print('full repair info entered Successfully')
+    
+    def clickStatusButton(self):
+        time.sleep(1)
+        self.driver.find_element(By.ID, "workingbut").click()        
+        print('status button clicked Successfully')
+
+    def runTest(self):
+        self.openBarcodePage()
+        self.enterBarcodeNumber()
+        self.fillTextBoxes()
+        self.logout_button()
+
+    def logout_button(self):
+            time.sleep(1)
+            logoutButton = self.driver.find_element_by_class_name('fa-sign-out')
+            logoutButton.click()
