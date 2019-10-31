606fd9f0c96790b30089890b59aa3c2a2f1854af
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2019-11-01 00:22:47 +0500

    Chrome Driver code cleanup

--- chromeWebDriver.py
+++ chromeWebDriver.py
@@ -2,10 +2,12 @@ from selenium import webdriver;
 
 class chromeWebDriver:
     def __init__(self):
-        print('constructor called chrome driver')
+        print('Chrome Driver open google chrome')
 
     def setchromeWebDriver(self):
-        self.wd = webdriver.Chrome('/usr/local/bin/chromedriver')
+        # Please added respective path your self
+        # on linux in terminal which chromeDriver
+        self.wd = webdriver.Chrome()
 
     def getWebDriver(self):
         return self.wd
